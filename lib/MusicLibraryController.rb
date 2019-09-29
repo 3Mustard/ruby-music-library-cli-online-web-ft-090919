6670d97354c1049d1996b297c070151d6977472f
Matthew C <m23.cullen@gmail.com>
2019-09-29 23:06:17 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -39,8 +39,11 @@ class MusicLibraryController
   
   def list_artists
     Artist.all.sort_by(&:name).each.with_index(1) do |artist,index|
-      
+      puts "#{index}. #{artist.name}"
+    end 
   end 
+  
+  
   
 end 
 
