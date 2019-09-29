ea6802809f8172bb6a14e9ab61f40be3cec3c304
Matthew C <m23.cullen@gmail.com>
2019-09-29 22:09:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,13 +38,13 @@ class Song
     @@all << self 
   end 
 
-  def self.find_by_name(name)
-    self.all.detect {|song| song.name == name}
-  end 
-  
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)
-  end 
+  #def self.find_by_name(name)
+   # self.all.detect {|song| song.name == name}
+  #end 
+
+  #def self.find_or_create_by_name(name)
+   # find_by_name(name) || create(name)
+  #end 
 end 
 
 
