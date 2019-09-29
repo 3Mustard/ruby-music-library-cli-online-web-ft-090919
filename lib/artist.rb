7c42254bfee115c31daedbf3b57d5c73c4032177
Matthew C <m23.cullen@gmail.com>
2019-09-29 21:26:48 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def add_song(song)
+    unless song.artist == self
+      song.artist = self 
+      @songs << song unless @songs.include?(song)
+    end 
+  end
+  
+  def genres 
+    songs.collect { |song| song.genre}.uniq
+  end
+end 
---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name 
+    self.genre=(genre) unless genre == nil
+    self.artist=(artist) unless artist == nil
+  end 
+  
+  def artist=(artist)
+    @artist = artist 
+    artist.add_song(self)
+  end 
+  
+  def genre=(genre)
+    @genre = genre 
+    genre.songs << self unless genre.songs.include?(self)
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self 
+  end 
+end 
